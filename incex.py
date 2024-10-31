565773cb9d56e1e0fe6a6e703e4428e1b72debc9
gemmameg <megijakalnina4@gmail.com>
2024-10-31 03:36:19 +0200

    Update and rename incex.py to index.html

--- incex.py
+++
@@ -1,8 +0,0 @@
-import random
-question = input('Do you want to dice Y/N : ')
-
-while question.upper() == 'Y':
-    print(f'your number is {random.randint(1,6)}')
-    question = input('Do you want to roll again !!')
-else:
-    print('Thank you For Playing')
